2d6aefae6a2425f3e79cd5ef5c26f928c09f499e
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-11-25 17:51:18 +0530

    add insert at tail

--- LinkedList/insertAtTail.cpp
+++ LinkedList/insertAtTail.cpp
@@ -5,39 +5,35 @@ class Node{
     public:
     int data;
     Node* next;
-
+    
     Node(int data){
         this->data=data;
         this->next=NULL;
-    } 
+    }
 };
 
-
 void insertAtTail(Node* &tail, int data){
-    Node* temp = new Node(data);
-    tail->next= temp;
-    tail = tail->next;
-
+    Node* temp= new Node(data);
+    tail->next=temp;
+    tail=tail->next;
 }
-
-void print(Node* &head){
-    Node* temp = head;
+void print(Node* &tail){
+    Node* temp=tail;
     while(temp!=NULL){
         cout<<temp->data<<" ";
-        temp = temp->next;
+        temp=temp->next;
     }
-    cout<<endl;
+    
 }
 
 int main(){
-
     Node* node1 = new Node(10);
-    Node* head = node1;
-    Node* tail = node1; 
-    print(head);
+    Node* tail = node1;
+    print(tail);
     insertAtTail(tail, 12);
+    print(tail);
+    insertAtTail(tail, 15);
+    print(tail);
     
-    print(head);
-
     return 0;
 }
